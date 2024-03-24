04b64da304856205a173a768042aafe2ad9f1b19
Firman Naufal <113032460+FirmanNaufal@users.noreply.github.com>
2024-03-25 00:19:43 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,95 @@
+# Firman Naufal Aryaputra
+# A11.2022.14181
+# LATIHAN 1
+
+
+print("Latihan1")
+print("Hello World!")
+print("test")
+
+print()
+
+print("Latihan The print() function")
+print("Hello, Python!")
+print("Firman")
+print('Firman')
+
+print()
+
+print("Formatting dengan Special Character")
+print("Saya sudah sarapan tadi pagi\n dan nanti siang makan lagi")
+print("Saya sudah sarapan tadi pagi\t dan nanti siang makan lagi")
+print("nama saya \"Firman\"")
+print("\"")
+print("satu.","dua.","tiga.")
+print("Nama saya","Firman", end=" ")
+print("Naufal")
+print("satu","dua","tiga", sep=", ")
+
+print()
+
+print("Latihan The Print Function")
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python"*2)
+
+print()
+print("Latihan Formatting the output")
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n"*2)
+
+print()
+
+print("Literal")
+print("2"*2)
+print(2*2)
+print(type("2"))
+print(type(2))
+
+print()
+
+print("Latihan Interger Vs Floats")
+print(5, "mempunyai tipe", type(5))
+print(.5, "mempunyai tipe", type(.5))
+
+print()
+print("Penulisan Float dan Integer")
+print (10_000_000)
+
+
+print()
+print("I'm Monty Python")
+
+
+print()
+print("Boolean")
+print(True)
+print(False)
+
+print(4>2)
+print(3>4)
+
+print()
+print("Latihan Variabel")
+print('"I\'m"\n""learning""\n"""Python"""')
+
+print()
+
+print("Latihan Python literals - strings")
+# membuat variabel Darjo, Darno, dan Waginem
+Darjo = 3
+Darno = 5
+Waginem = 6
+
+# mencetak nama variabel dan jumlah apel setiap variabel dalam satu baris, dipisahkan dengan koma
+print(f"{Darjo}, {Darno}, {Waginem}")
+
+# membuat variabel baru totalApel yang berisi penjumlahan seluruh apel yang mereka miliki
+totalApel = Darjo + Darno + Waginem
+print("Total apel:", totalApel)
+
+# mencari rata-rata jumlah apel
+rata_rata_apel = totalApel / 3
+print("Rata-rata apel:", rata_rata_apel)
+
+
+print()
+
